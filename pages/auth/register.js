a0b364f83f8cd6ec28ac2164883bb94e33b1ecca
Ganindu Dhananja <ganindudananja@gmail.com>
2021-10-02 10:20:46 +0530

    Register page added

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 // reactstrap components
 import {
   Button,
@@ -18,107 +18,254 @@ import {
 // layout for this page
 // import Auth from 'layouts/Auth.js';
 import Guest from 'layouts/Guest';
+import { register } from 'lib/api';
 
 function Register() {
+  const [state, update] = useState({
+    user: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      login: '',
+      password: '',
+      confirmpassword: ''
+    },
+    validations: {
+      email: false,
+      login: false,
+      password: false,
+      confirmpassword: false
+    },
+    formValidity: false
+  });
+  const router = useRouter();
+
+  const setState = (updatedState) => {
+    update({
+      ...state,
+      ...updatedState
+    });
+  };
+
+  const handleChange = (e) => {
+    const target = e.target;
+    let updatedUser = state.user;
+    let updatedValidations = state.validations;
+    let validity = true;
+    updatedUser[target.id] = target.value;
+    if (updatedValidations.hasOwnProperty(target.id)) {
+      updatedValidations[target.id] = validateField(target.id, target.value);
+    }
+    for (const key in state.validations) {
+      validity = validity && state.validations[key];
+    }
+    setState({
+      user: updatedUser,
+      validations: updatedValidations,
+      formValidity: validity
+    });
+  };
+
+  const validateField = (id, value) => {
+    switch (id) {
+      case 'email':
+        const emailTest = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        return emailTest.test(value);
+      case 'password':
+        return value.length >= 8;
+      case 'confirmpassword':
+        return state.user.password === value;
+      default:
+        return value.trim() !== '';
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (state.formValidity) {
+      const user = {
+        firstname: state.user.firstName,
+        lastname: state.user.lastName,
+        email: state.user.email,
+        username: state.user.login,
+        password: state.user.password,
+        confirmed: true,
+        role: 'Authenticated'
+      };
+      // console.log(state, user);
+      const response = await register(user);
+      // console.log(response);
+      if (response.error) {
+        console.error(response.error);
+      } else if (
+        !!response &&
+        (response.status === 200 || response.statusText === 'OK')
+      ) {
+        router.push('/auth/login');
+      }
+    }
+  };
+
   return (
     <>
       <Col lg="6" md="8">
         <Card className="bg-secondary shadow border-0">
-          <CardHeader className="bg-transparent pb-5">
-            <div className="text-muted text-center mt-2 mb-4">
-              <small>Sign up with</small>
-            </div>
-            <div className="text-center">
-              <Button
-                className="btn-neutral btn-icon mr-4"
-                color="default"
-                href="#"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="btn-inner--icon">
-                  <img alt="..." src={require('assets/img/icons/common/github.svg')} />
-                </span>
-                <span className="btn-inner--text">Github</span>
-              </Button>
-              <Button
-                className="btn-neutral btn-icon"
-                color="default"
-                href="#"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="btn-inner--icon">
-                  <img alt="..." src={require('assets/img/icons/common/google.svg')} />
-                </span>
-                <span className="btn-inner--text">Google</span>
-              </Button>
-            </div>
+          <CardHeader className="bg-transparent text-center">
+            <h3>Sign up with FinancialVotes.com now!</h3>
           </CardHeader>
           <CardBody className="px-lg-5 py-lg-5">
-            <div className="text-center text-muted mb-4">
-              <small>Or sign up with credentials</small>
-            </div>
-            <Form role="form">
+            <Form role="form" onSubmit={handleSubmit}>
+              <Row>
+                <Col md={6}>
+                  <FormGroup>
+                    <InputGroup className="input-group-alternative">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="ni ni-circle-08" />
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        id="firstName"
+                        placeholder="First Name"
+                        type="text"
+                        value={state.user.firstName}
+                        onChange={handleChange}
+                      />
+                    </InputGroup>
+                  </FormGroup>
+                </Col>
+                <Col md={6}>
+                  <FormGroup>
+                    <InputGroup className="input-group-alternative">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="ni ni-circle-08" />
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        id="lastName"
+                        placeholder="Last Name"
+                        type="text"
+                        value={state.user.lastName}
+                        onChange={handleChange}
+                      />
+                    </InputGroup>
+                  </FormGroup>
+                </Col>
+              </Row>
               <FormGroup>
-                <InputGroup className="input-group-alternative mb-3">
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="ni ni-hat-3" />
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input placeholder="Name" type="text" />
-                </InputGroup>
-              </FormGroup>
-              <FormGroup>
-                <InputGroup className="input-group-alternative mb-3">
+                <InputGroup className="input-group-alternative">
                   <InputGroupAddon addonType="prepend">
                     <InputGroupText>
                       <i className="ni ni-email-83" />
                     </InputGroupText>
                   </InputGroupAddon>
-                  <Input placeholder="Email" type="email" autoComplete="new-email" />
+                  <Input
+                    id="email"
+                    placeholder="Email Address"
+                    type="email"
+                    value={state.user.email}
+                    onChange={handleChange}
+                  />
                 </InputGroup>
+                <div className="text-muted font-italic">
+                  <small>
+                    {state.validations.email ? null : (
+                      <span className="text-info font-weight-700">
+                        Valid email address is required!
+                      </span>
+                    )}
+                  </small>
+                </div>
               </FormGroup>
               <FormGroup>
                 <InputGroup className="input-group-alternative">
                   <InputGroupAddon addonType="prepend">
                     <InputGroupText>
-                      <i className="ni ni-lock-circle-open" />
+                      <i className="ni ni-single-02" />
                     </InputGroupText>
                   </InputGroupAddon>
                   <Input
-                    placeholder="Password"
-                    type="password"
-                    autoComplete="new-password"
+                    id="login"
+                    placeholder="Username"
+                    type="text"
+                    value={state.user.login}
+                    onChange={handleChange}
                   />
                 </InputGroup>
-              </FormGroup>
-              <div className="text-muted font-italic">
-                <small>
-                  password strength:{' '}
-                  <span className="text-success font-weight-700">strong</span>
-                </small>
-              </div>
-              <Row className="my-4">
-                <Col xs="12">
-                  <div className="custom-control custom-control-alternative custom-checkbox">
-                    <input
-                      className="custom-control-input"
-                      id="customCheckRegister"
-                      type="checkbox"
-                    />
-                    <label className="custom-control-label" htmlFor="customCheckRegister">
-                      <span className="text-muted">
-                        I agree with the{' '}
-                        <a href="#" onClick={(e) => e.preventDefault()}>
-                          Privacy Policy
-                        </a>
+                <div className="text-muted font-italic">
+                  <small>
+                    {state.validations.login ? null : (
+                      <span className="text-info font-weight-700">
+                        Username is required!
                       </span>
-                    </label>
-                  </div>
+                    )}
+                  </small>
+                </div>
+              </FormGroup>
+              <Row>
+                <Col md={6}>
+                  <FormGroup>
+                    <InputGroup className="input-group-alternative">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="ni ni-lock-circle-open" />
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        id="password"
+                        placeholder="Password"
+                        type="password"
+                        value={state.user.password}
+                        onChange={handleChange}
+                      />
+                    </InputGroup>
+                    <div className="text-muted font-italic">
+                      <small>
+                        Password strength:{' '}
+                        {state.validations.password ? (
+                          <span className="text-success font-weight-700">strong</span>
+                        ) : (
+                          <span className="text-danger font-weight-700">weak</span>
+                        )}
+                      </small>
+                    </div>
+                  </FormGroup>
+                </Col>
+                <Col md={6}>
+                  <FormGroup>
+                    <InputGroup className="input-group-alternative">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="ni ni-lock-circle-open" />
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        id="confirmpassword"
+                        placeholder="Confirm Password"
+                        type="password"
+                        value={state.user.confirmpassword}
+                        onChange={handleChange}
+                      />
+                    </InputGroup>
+                    <div className="text-muted font-italic">
+                      <small>
+                        {state.validations.confirmpassword ? (
+                          <span className="text-success font-weight-700">
+                            Passwords match!
+                          </span>
+                        ) : (
+                          <span className="text-info font-weight-700">
+                            Passwords should be matched!
+                          </span>
+                        )}
+                      </small>
+                    </div>
+                  </FormGroup>
                 </Col>
               </Row>
               <div className="text-center">
-                <Button className="mt-4" color="primary" type="button">
+                <Button className="mt-4" color="primary" type="submit">
                   Create account
                 </Button>
               </div>
